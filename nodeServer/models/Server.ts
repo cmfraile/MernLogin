a26940d12504b69af5cd88037e8325727cda6e89
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-02-16 14:49:46 +0100

    añadido el backensuelo

---
+++ nodeServer/models/Server.ts
@@ -0,0 +1,58 @@
+import express , { Application, Router } from 'express';
+import { dataBaseConnection } from '../database/configDB';
+import cors from 'cors';
+import fileUpload from 'express-fileupload';
+
+//import * as controllers from '../controllers';
+//const controllersIndexed:{[key:string]:any} = {...controllers}
+
+class Server {
+
+    private app:Application;
+    private port:string;
+
+    constructor(){
+        this.app    =   express() ;
+        this.port   =   process.env.PORT || '8000' ;
+        this.middlewares();
+        //this.databaseTasks();
+        //this.routes();
+    }
+
+    private middlewares(){
+        this.app.use(express.json());
+        this.app.use(cors());
+        this.app.options('*',cors());
+        this.app.use(fileUpload({useTempFiles:true,tempFileDir:'/tmp/',createParentPath:true}));
+    };
+
+    private async databaseTasks(){
+        await dataBaseConnection();
+    };
+
+    //private routes(){}
+
+    public listen(){ this.app.listen(this.port) ; console.log('Backend ON') };
+
+}
+
+export default Server ;
+
+/*
+    private routes(){
+
+        let pathCrafter:{[key:string]:{route:string,router:Router}} = {} ;
+        apiPointer.map(x => {
+            pathCrafter[x] = {
+                route:`/api/${x}`,
+                router:controllersIndexed[`${x}Router`]
+            }
+        });
+
+        Object.keys(pathCrafter).map(x => {
+            const { route , router } = pathCrafter[x];
+            this.app.use( route , router );
+        });
+
+    }
+    */
