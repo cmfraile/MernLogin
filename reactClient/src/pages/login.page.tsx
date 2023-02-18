66799ac02cb95cbcb4934e7412100563222235db
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-02-18 12:06:32 +0100

    el boton de GoogleLogin estaba dando otros datos, pero no el acceso debidamente

--- reactClient/src/pages/login.page.tsx
+++ reactClient/src/pages/login.page.tsx
@@ -1,21 +1,19 @@
-import { GoogleLogin } from '@react-oauth/google';
+import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import { useContext } from 'react';
 import { authContext } from '../auth/context/auth.context';
 
 const LoginOrRegister = () => {
 
     const { fetchState , setFetchProps , getFetch } = useContext(authContext).authFetch ;
-
-    const onSuccess = ({credential}:any) => {console.log(credential)};
+    const onSuccess = (resp:any) => {console.log(resp)};
+    const login = useGoogleLogin({onSuccess})
 
     return(
         <div className="container">
 
             <div className="row"><div className="col">
                 <h2>Login or Register</h2>
-                <GoogleLogin 
-                    onSuccess={onSuccess}
-                    size={'large'}/>
+                <button onClick={() => {login()}} >Lorem ipsum dolor sit amet.</button>
             </div></div>
 
         </div>
