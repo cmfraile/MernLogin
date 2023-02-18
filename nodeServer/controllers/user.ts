ef299d2ae61e9bb0715c755b67d13a2c8a82a80a
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-02-18 15:49:59 +0100

    ya logramos obtener los datos de quien logea

--- nodeServer/controllers/user.ts
+++ nodeServer/controllers/user.ts
@@ -1,24 +1,30 @@
 import { Router } from "express";
 import { Request , Response } from "express";
+import axios from "axios";
 import * as jwt from 'jsonwebtoken';
 
-interface user { token:string } ;
+const secret:any = process.env.JWTKEY;
 
-const user = async(req:Request,res:Response) => {
+const userInfoQuery = async(token:string):Promise<string> => await axios.get("https://www.googleapis.com/oauth2/v3/userinfo",{headers: {"Authorization": `Bearer ${token}`}});
+
+const googleUser = async(req:Request,res:Response) => {
+
     try{
-
         const token = req.header('token');
-        if(token){
-            jwt.
-        }
+        if(!token){return res.status(403).json({error:'el token de usuario de google no se encuentra o no es válido'})};
+        await userInfoQuery(token).then(console.log);
+        return res.status(200).json({});
+    }catch(err){return res.status(500).json()};
 
-        return res.status(200).json({logged:true})
-    }catch(err){return res.status(500).json({logged:false})}
 }
 
 const userRouter = Router();
-
-userRouter.get('/',[],user);
+userRouter.post('/google',[],googleUser)
 
 export { userRouter };
+
+/*
+userRouter.get('/',[],checkOldToken);
+userRouter.get('/',[],registerOrLogin);
+*/
 
