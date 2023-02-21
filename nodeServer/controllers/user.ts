1160368acd47fe01acce7931b7977f1e5bd1afeb
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-02-21 11:48:09 +0100

    vamos a hacer pruebas

--- nodeServer/controllers/user.ts
+++ nodeServer/controllers/user.ts
@@ -3,26 +3,28 @@ import { Request , Response } from "express";
 import axios from "axios";
 import * as jwt from 'jsonwebtoken';
 
-const userInfoQuery = async(token:string):Promise<any> => axios.get("https://www.googleapis.com/oauth2/v3/userinfo",{headers: {"Authorization": `Bearer ${token}`}});
-const googleUser = async(req:Request,res:Response) => {
-
+interface decode {email:string,given_name:string,picture:string}
+const jwtSign = ({email,given_name,picture}:decode) => {
     const secret:any = process.env.JWTKEY;
+    const token = jwt.sign({email,given_name,picture},secret,{expiresIn:'1h'});
+    const bundle = {
+        user:{email,name:given_name,picture},
+        token
+    };
+    return bundle
+};
+const userInfoQuery = async(token:string):Promise<any> => axios.get("https://www.googleapis.com/oauth2/v3/userinfo",{headers: {"Authorization": `Bearer ${token}`}});
+
+const googleUser = async(req:Request,res:Response) => {
+    
     const tokenError = () => res.status(403).json({error:'el token de usuario de google no se encuentra o no es válido'});
-    const jwtSign = ({email,given_name,picture}:any) => {
-        const token = jwt.sign({email,given_name,picture},secret,{expiresIn:'1h'});
-        const bundle = {
-            user:{email,name:given_name,picture},
-            token
-        };
-        return res.status(200).json(bundle);
-    }
 
     try{
 
         const token = req.header('token');
         if(!token){return tokenError()};
         userInfoQuery(token)
-        .then(({data}) => {jwtSign(data)})
+        .then(({data}) => {return res.status(200).json(jwtSign(data))})
         .catch(console.log);
 
     }catch(err){return res.status(500).json()};
@@ -35,17 +37,17 @@ const checkToken = async(req:Request,res:Response) => {
     const token = req.header('token');
 
     try{
-        jwt.verify(`${token}`,secret,(err:any,decode:any) => {
-            if(err){return res.status(200).json({validToken:true})};
-            if(decode){return res.status(200).json({validToken:false})};
+        jwt.verify(`${token}`,secret,(err:any,{user,token}:any) => {
+            if(err){return res.status(403).json({validToken:false})};
+            if(token){return res.status(200).json(jwtSign(user))};
         })
     }catch(err){return res.status(500).json()}
 
 }
 
 const userRouter = Router();
-userRouter.post('/google',[],googleUser)
-userRouter.get('/checkToken',[],checkToken)
+userRouter.post('/google',[],googleUser);
+userRouter.post('/checkToken',[],checkToken);
 
 export { userRouter };
 
