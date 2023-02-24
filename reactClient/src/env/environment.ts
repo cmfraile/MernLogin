cc811cd5508ae52c1e8f72e50ceacf1485f9e265
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-02-24 09:44:23 +0100

    prueba

--- reactClient/src/env/environment.ts
+++
@@ -1,6 +0,0 @@
-const environment = {
-    backendURL:'http://localhost:8000/api/',
-    googleClientID:'264198359560-toopp10htfi62063esg6877dnm72gd5m.apps.googleusercontent.com'
-}
-
-export default environment
